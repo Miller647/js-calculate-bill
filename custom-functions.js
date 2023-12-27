0a521df8075ee173cd03fa6c6851bacb41ebd7ab
Natasha Miller <66027047+Miller647@users.noreply.github.com>
2023-12-27 12:54:13 -0500

    add parameters to function

--- custom-functions.js
+++ custom-functions.js
@@ -1,16 +1,19 @@
 // Function Definition
-function calculateBill() {
+// Parameters = placeholders
+
+function calculateBill(billAmount, taxRate) {
   // this is the body of the function
   console.log("Running Calculate Bill Function");
-  const total = 100 * 1.13;
+  const total = billAmount * (1 + taxRate);
   // temporary variable only available inside of the function
   return total;
 }
 
-// Function Call or Running a Function
+// Call, Run or Invoke Function
 const myTotal = calculateBill();
 // console.log(`Your total is $${myTotal}`);
 
 // Or could interpolate the function calculateBill() in the console.log
+// Arguments = actual values
 
-console.log(`Your total is $${calculateBill()}`);
+console.log(`Your total is $${calculateBill(100, 0.13)}`);
